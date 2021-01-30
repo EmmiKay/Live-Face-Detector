481b0f98b1c0af311cd1787ca6d641b3445d5765
Emily Brantner <embrantner@gmail.com>
2021-01-30 18:39:17 -0500

    added_files

--- cam.py
+++ cam.py
@@ -1,5 +1,18 @@
 import numpy as np
 import cv2
+import paho.mqtt.client as mqtt
+
+MQTT_HOST = 'nx_broker'
+
+MQTT_PORT = 1883
+MQTT_TOPIC = 'hw3'
+
+def on_connect(client, userdata, flags, rc):
+    print('connected with result code {0}'.format(str(rc)))
+
+mclient = mqtt.Client()
+mqttclient.on_connect = on_connect
+
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 #could be something other than 0 depending on camera
@@ -14,6 +27,10 @@ while(True):
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for (x,y,w,h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        print('face detected', face.shape, face.dtype)
+        rc, jpg = cv.lmencode(*png, face)
+        msg = jpg.tobytes()
+        mclient.publish(MQTT_TOPIC, payload = msg, qos = 2, retain = False)
 
     #display the frame
     cv2.imshow('frame', frame)
@@ -22,5 +39,5 @@ while(True):
         break
 
 #release the captures
-cap.relese()
+cap.release()
 cv2.destroyAllWindows()
