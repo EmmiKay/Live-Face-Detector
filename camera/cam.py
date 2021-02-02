c4ad6bf394e4f1dd975b7d3bf8833447eed32852
Emily Brantner <embrantner@gmail.com>
2021-02-02 01:37:37 -0500

    code changes

--- camera/cam.py
+++ camera/cam.py
@@ -3,42 +3,47 @@ import cv2
 import paho.mqtt.client as mqtt
 import os
 
-MQTT_HOST = 'nx_broker'
+MQTT_BROKER = 'nx_broker'
 MQTT_PORT = 1883
 MQTT_TOPIC = 'hw3'
 
-def on_connect(client, userdata, flags, rc):
-    print('connected with result code {0}'.format(str(rc)))
+def on_connect_local(client, userdata, flags, rc):
+    print("connected to local broker with rc: " + str(rc))
+    main()
 
-mclient = mqtt.Client()
-mclient.on_connect = on_connect
-mclient.connect(MQTT_HOST, MQTT_PORT, 60)
+def main():
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    #could be something other than 0 depending on camera
+    cap = cv2.VideoCapture(0)
 
-#could be something other than 0 depending on camera
-cap = cv2.VideoCapture(0)
+    while(True):
+        #get frame by frame captures
+        ret, frame = cap.read()
 
-while(True):
-    #get frame by frame captures
-    ret, frame = cap.read()
+        #operations on the frame go here
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        for (x,y,w,h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            print('face detected', frame.shape, frame.dtype)
+            rc, png = cv2.imencode('.png', frame)
+            msg = png.tobytes()
+            mqttclient.publish(MQTT_TOPIC, payload = msg, qos = 1, retain = False)
 
-    #operations on the frame go here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    for (x,y,w,h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        print('face detected', frame.shape, frame.dtype)
-        rc, png = cv2.imencode('.png', frame)
-        msg = png.tobytes()
-        mclient.publish(MQTT_TOPIC, payload = msg, qos = 2, retain = False)
+        #display the frame
+        cv2.imshow('frame', frame)
 
-    #display the frame
-    cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    #release the captures
+    cap.release()
 
-#release the captures
-cap.release()
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    mqttclient = mqtt.Client()
+    mqttclient.on_connect = on_connect_local
+    mqttclient.connect(MQTT_BROKER, MQTT_PORT, 60)
+
+    mqttclient.loop_forever()
+    cv2.destroyAllWindows()
--- forwarder/forwarder.py
+++ forwarder/forwarder.py
@@ -10,14 +10,11 @@ CLOUD_MQTT_HOST = 'ec2-18-191-197-152.us-east-2.compute.amazonaws.com'
 CLOUD_MQTT_PORT = 1883
 CLOUD_MQTT_TOPIC = 'faces'
 
-def on_connect(client, userdata, flags, rc):
+def on_connect_local(client, userdata, flags, rc):
     print("connected to local broker with rc: " + str(rc))
 
 def on_connect_cloud(client, userdata, flags, rc):
-    try:
         print('connected to cloud')
-    except:
-        print("Unexpected error:", sys.exc_info()[0])
 
 def on_message(client,userdata, msg):
     print('on message received')
@@ -26,7 +23,7 @@ def on_message(client,userdata, msg):
 
 mqttclient = mqtt.Client()
 mqttclient.connect(MQTT_HOST, MQTT_PORT, 60)
-mqttclient.on_connect = on_connect
+mqttclient.on_connect = on_connect_local
 
 mqttclient.subscribe(MQTT_TOPIC, qos=2)
 mqttclient.on_message = on_message
@@ -35,6 +32,4 @@ cloud_mqttclient = mqtt.Client()
 cloud_mqttclient.connect(CLOUD_MQTT_HOST, CLOUD_MQTT_PORT, 60)
 cloud_mqttclient.on_connect = on_connect_cloud
 
-
-
-local_mqttclient.loop_forever()
+mqttclient.loop_forever()
